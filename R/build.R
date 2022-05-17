0409183c71cefb0c9b48eb200d7e8fc2b6c6c644
Matt Crump <crumpmj@gmail.com>
2022-05-17 14:46:22 -0400

    update blog cards

--- R/build.R
+++ R/build.R
@@ -1,6 +1,29 @@
-crumplab_blog <- function(){
-  pkgdown::build_articles(override = yaml::read_yaml("_blog.yml"))
+#' create index.html for articles blog page
+#'
+#' @param pkg
+#' @param override
+#'
+#' @return
+#' @export
+#'
+#' @examples
+crumplab_blog_index <- function(pkg=".",override = yaml::read_yaml("_blog.yml")){
+  pkg <- pkgdown:::section_init(pkg, depth = 1L, override = override)
+  pkgdown::build_articles_index(pkg)
 }
+
+#' build blog pages
+#'
+#' @return
+#' @export
+#'
+#' @examples
+crumplab_blog <- function(){
+  pkgdown::build_articles()
+  crumplab_blog_index()
+}
+
+
 
 
 
